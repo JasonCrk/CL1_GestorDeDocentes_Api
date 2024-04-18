76bfbd5d6c4edb04077a0bea3f98075ac6cf835d
JasonCrk <emerzonkolki@gmail.com>
2024-04-18 00:09:48 -0500

    feat: initial commit

---
+++ src/main/java/com/CL1/GestorDeDocentes/GestorDeDocentesRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.CL1.GestorDeDocentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeDocentesRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeDocentesRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CL1/GestorDeDocentes/GestorDeDocentesRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CL1.GestorDeDocentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeDocentesRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
