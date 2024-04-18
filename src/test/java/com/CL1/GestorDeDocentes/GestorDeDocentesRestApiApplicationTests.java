3fb9a0b105845e9a3400b62be4470632c0c51b96
JasonCrk <emerzonkolki@gmail.com>
2024-04-18 17:18:42 -0500

    test: Add setup

--- src/test/java/com/CL1/GestorDeDocentes/GestorDeDocentesRestApiApplicationTests.java
+++ src/test/java/com/CL1/GestorDeDocentes/GestorDeDocentesRestApiApplicationTests.java
@@ -1,9 +1,14 @@
 package com.CL1.GestorDeDocentes;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
+@ExtendWith(SpringExtension.class)
 class GestorDeDocentesRestApiApplicationTests {
 
 	@Test
