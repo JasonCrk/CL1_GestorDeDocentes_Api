546bd0920a5e3605656c62e47b8ef2ba1f969332
JasonCrk <emerzonkolki@gmail.com>
2024-04-19 12:13:56 -0500

    feat(main/resources): Default data from sql script

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO categoria (nombre) VALUES ('Principal');
+INSERT INTO categoria (nombre) VALUES ('Asociado');
+INSERT INTO categoria (nombre) VALUES ('Auxiliar');
+
+INSERT INTO docente (nombre, dni, fecha_nacimiento, sueldo, email, sexo, idcategoria) VALUES ('Juliana Rodriguez', '62453446', '1995-04-05', 2000.00, 'docente1@gmail.com', 'Mujer', 1);
+INSERT INTO docente (nombre, dni, fecha_nacimiento, sueldo, email, sexo, idcategoria) VALUES ('Pablo Iglesias', '79053433', '1995-04-05', 2000.00, 'docente2@gmail.com', 'Hombre', 2);
+INSERT INTO docente (nombre, dni, fecha_nacimiento, sueldo, email, sexo, idcategoria) VALUES ('Mario de las Casas', '72453446', '1995-04-05', 2000.00, 'docente3@gmail.com', 'Hombre', 1);
+INSERT INTO docente (nombre, dni, fecha_nacimiento, sueldo, email, sexo, idcategoria) VALUES ('Jhon Alexander Loyola Diaz', '93342556', '1990-04-05', 8000.00, 'loyolaDiaz@gmail.com', 'Hombre', 3);
