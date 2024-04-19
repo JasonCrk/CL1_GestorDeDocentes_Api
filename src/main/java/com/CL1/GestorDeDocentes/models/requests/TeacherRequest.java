dce3c68c1de31b7e9fbd16179fc6aee101bb4d5d
JasonCrk <emerzonkolki@gmail.com>
2024-04-19 12:08:14 -0500

    feat(TeacherRequest.java): Improve validations

--- src/main/java/com/CL1/GestorDeDocentes/models/requests/TeacherRequest.java
+++ src/main/java/com/CL1/GestorDeDocentes/models/requests/TeacherRequest.java
@@ -1,8 +1,9 @@
 package com.CL1.GestorDeDocentes.models.requests;
 
 import com.CL1.GestorDeDocentes.models.enums.Gender;
+import com.CL1.GestorDeDocentes.models.validations.IsDni;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -10,11 +11,26 @@ import java.time.LocalDate;
 public record TeacherRequest(
         @NotBlank(message = "El nombre es requerido")
         String name,
+
+        @IsDni
+        @NotBlank(message = "El DNI es requerido")
         String dni,
+
+        @NotNull(message = "La fecha de nacimiento es requerido")
         LocalDate birthdate,
+
+        @DecimalMin(value = "0.0", inclusive = false)
+        @NotNull(message = "El salario es requerido")
         BigDecimal salary,
+
+        @Email(message = "El email es invalido")
         String email,
+
+        @NotNull(message = "El genero es requerido")
         Gender gender,
+
+        @NotNull(message = "El ID de la categoria es requerido")
+        @Min(value = 1)
         int categoryId
 ) {
 }
